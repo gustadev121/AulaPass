24fc9e925be39efcea4dc322e035e4a679060a23
christianmz565 <christianmz565@gmail.com>
2026-05-24 21:21:53 -0500

    feat: prepare test catalog

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bun
+            nodejs
+          ];
+          buildInputs = [ pkgs.bashInteractive ];
+          shellHook = "";
+        };
+      }
+    );
+}
